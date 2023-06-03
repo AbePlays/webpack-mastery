f5364d362a23badc1d86fff7758e88022824572d
Abhishek Rawat <abhi.rawat456@gmail.com>
2023-06-03 12:14:35 +0530

    feat: add `style-loader`, `css-loader`

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,14 @@ const path = require('path')
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
