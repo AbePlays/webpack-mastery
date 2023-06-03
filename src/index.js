4883bb1d61acb1f2745ab2734622c1f13b1f40fc
Abhishek Rawat <abhi.rawat456@gmail.com>
2023-06-03 07:35:02 +0530

    feat: add vanilla starter files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-function addNumbers(a, b) {
-  const sum = a + b
-  return sum
+const form = document.getElementById('add-form')
+const output = document.getElementById('result')
+
+function sum(nums) {
+  const result = nums.reduce((acc, num) => acc + num, 0)
+  return result
 }
 
-module.exports = { addNumbers }
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const { first, second } = e.target.elements
+  const sum = sum([+first.value, +second.value])
+  output.innerText = `Total = ${sum}`
+})
