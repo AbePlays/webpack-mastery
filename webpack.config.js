5b47aca566e2989f1be2c405d84f1ade767f256e
Abhishek Rawat <abhi.rawat456@gmail.com>
2023-06-03 11:28:37 +0530

    feat: add `html-webpack-plugin`

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
@@ -6,4 +7,10 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+  ],
 }
