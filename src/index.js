9112fb23fb85689a02de6bb41bd43e3eeb025b75
Abhishek Rawat <abhi.rawat456@gmail.com>
2023-06-07 08:45:48 +0530

    feat: tree shaking with webpack

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import './index.css'
 const form = document.getElementById('add-form')
 const output = document.getElementById('result')
 
+const foo = 'bar'
+
+function multiply(nums) {
+  const result = nums.reduce((acc, num) => acc * num, 1)
+  return result
+}
+
 function sum(nums) {
   const result = nums.reduce((acc, num) => acc + num, 0)
   return result
