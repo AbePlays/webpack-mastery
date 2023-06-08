710dc6a39916acc7a086b7ca2c6a9c5c134d21d7
Abhishek Rawat <abhi.rawat456@gmail.com>
2023-06-08 09:10:19 +0530

    fix: create the dist folder in root dir

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../dist'),
   },
   plugins: [
     new HtmlWebpackPlugin({
