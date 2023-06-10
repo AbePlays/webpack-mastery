6e36caee8bfb63af7559fa71603eb18663942ddf
Abhishek Rawat <abhi.rawat456@gmail.com>
2023-06-10 12:58:31 +0530

    feat: add `webpack-dev-server`

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -2,5 +2,11 @@ const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
+  devServer: {
+    hot: true,
+    open: true,
+    port: 3000,
+    static: ['src'],
+  },
   mode: 'development',
 })
